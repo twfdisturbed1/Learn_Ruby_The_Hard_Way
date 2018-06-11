f83b1cc0168b3114cdc1639e8e7587665dfe7f88
William Mullins <twfdisturbed@hotmail.com>
2018-06-11 13:48:43 -0400

    5 and 5b

---
+++ ex05b.rb
@@ -0,0 +1,29 @@
+# This line is setting the name.
+    name = 'Zed A. Shaw'
+# This line is setting the age.
+    age = 35 #not a lie in 2009
+# This line is setting the height.
+    height = 74 # inches
+# This line is setting the weight.
+    weight = 180 #lbs
+# This line is setting eye colors.
+    eyes = 'blue'
+# This line is setting teeth color.
+    teeth = 'white'
+# This line is setting hair color.
+    hair = 'brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} and #{hair} hair."
+puts "His teeth are usally #{teeth} depending on the coffee."
+
+# This line is tricky,to get exactly right.
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "If I wasn' in America then my weight would be #{weight * 0.453592}kg."
+
+puts "Also my height would be #{height * 2.54}cm."
