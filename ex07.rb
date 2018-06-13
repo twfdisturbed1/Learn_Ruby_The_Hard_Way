e15f25ed123af338c0d2077e8729b9591c445755
William Mullins <twfdisturbed@hotmail.com>
2018-06-13 13:22:53 -0400

    EX07 uploaded

---
+++ ex07.rb
@@ -0,0 +1,40 @@
+# Displays  Mary had a little lamb.
+puts "Mary had a little lamb."
+# Displays Its fleece was white as #{'snow'}
+puts "Its fleece was white as #{'snow'}"
+# Displays And everywhere that Mary went
+puts "And everywhere that Mary went."
+# Displays . 10 times
+puts "." * 10 #what'd that do?
+
+# Sets end1 to C
+end1 = "C"
+# Sets end2 to h
+end2 = "h"
+# Sets end3 to e
+end3 = "e"
+# Sets end4 to e
+end4 = "e"
+# Sets end5 to s
+end5 = "s"
+# Sets end6 to e
+end6 = "e"
+# Sets end7 to B
+end7 = "B"
+# Sets end8 to u
+end8 = "u"
+# Sets end9 to r
+end9 = "r"
+# Sets end10 to g
+end10 = "g"
+# Sets end11 to e
+end11 = "e"
+# Sets end12 to r 
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# Print displays end1 - end6
+# By using Print before puts it keeps them on the same line
+# Displays end7 - end12
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
