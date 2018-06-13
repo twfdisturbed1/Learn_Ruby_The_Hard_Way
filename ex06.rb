532951acf673c04da935c8546c7985469998a568
William Mullins <twfdisturbed@hotmail.com>
2018-06-13 10:28:15 -0400

    added ex 06

---
+++ ex06.rb
@@ -0,0 +1,32 @@
+# Sets types _of_people to 10.
+types_of_people = 10
+# Set X to a string with types _of_people in it.
+x = "There are #{types_of_people} types of people."
+# Sets binary to bianry.
+binary = "binary"
+# Sets do_not to don't.
+do_not = "don't"
+# Sets y to a string witth binary and do_not.
+y = "Those who know #{binary} and those who #{do_not}."
+# Displays X then y.
+puts x
+puts y
+# Displays a string with x in it.
+puts "I said: #{x}."
+# Displays a string with y in it.
+puts "I also said: '#{y}'."
+
+# Sets hilarious to false.
+hilarious = false
+# Sets joke_evaluation to a string and calls on hilarious.
+joke_evaluation = "Isn't that joke so funny? ! #{hilarious}"
+
+# Displays joke_evaluation.
+puts joke_evaluation
+
+# Sets w to a string of text.
+w = "This is the left side of..."
+# Sets e to a string of text.
+e = "a string  with a right side."
+# Displays w then e
+puts w + e
